43ca7c2f069388686616f90db08604f04307aee2
Ana <anafp0518@gmail.com>
2026-06-23 13:40:24 +0000

    ...

--- app.py
+++ app.py
@@ -1,14 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_templeate
 
 app = Flask(__name__)
 
 @app.route('/')
 def inicio():
-    nome = "Turma de Programação"
-    curso = "Python com HTML"
+nome = "Turma de Programação"
+curso = "Phyton com HTML"
 
-    return render_template(
-        'index.html'
-        nome = nome,
-        curso = curso
-    )
+return render_templeate(
+'index.html',
+nome = nome,
+curso = curso
+)
+
+@app.route('/sobre')
+def sobre():
+return
+"""
+<H1>Sobre o Projeto</H1>
+<p>Este código foi criado com Phyton e Flask.</p>
+<a href="/"> Volta para i inicio<a>
+"""
+if__name__=='__main__':
+app.run(host='0.0.0.0', port=3000, debug=True)
